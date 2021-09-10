1e380f6a662836b3cdeea04e0ce046322c67399e
Tejas268 <83995723+Tejas268@users.noreply.github.com>
2021-09-10 12:37:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,6 +7,8 @@ var plinkos = [];
 var divisions =[];
 var score = 0;
 
+var currentPlinko = 0;
+
 var z = 10;
 var d = 50;
 var n = 75;
@@ -18,7 +20,6 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
-
   //create division objects
   for (var k = 0; k <= width; k = k + 80) {
     divisions.push(new Divisions(k, 650, 10, 300));
@@ -49,23 +50,17 @@ function draw() {
   Engine.update(engine);
   ground.display();
   
-  if (frameCount % 60 === 0) {
-    plinkos.push(new Plinko(400, -21));
+  if (frameCount % 80 === 0) {
+    var px = random(30, 770);
+    plinkos.push(new Plinko(px, -21, random(0, 255), random(0, 255), random(0, 255)));
     Matter.Body.setStatic(plinkos[a].body, false);
     a++;
   }
 
   //display the plinkos 
   for (var i = 0; i < plinkos.length; i++) {
-    if (i < 63) {
-      plinkos[i].display();
-    }
-    else if (i < 62) {
-      push();
-      RGB(random(0,255),random(0,255),random(0,255)));
-      plinkos[i].display();
-      pop();
-    }
+    currentPlinko = i;
+    plinkos[i].display();
   }
    
   //display the divisions
